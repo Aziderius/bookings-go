9a35cdcbb35fe6a2ca778ed34ae6f56d63c10285
Aziderius <143766082+Aziderius@users.noreply.github.com>
2024-10-16 12:21:00 -0700

    first commit, initial web application

---
+++ pkg/render/render.go
@@ -0,0 +1,147 @@
+package render
+
+import (
+	"github.com/aziderius/bookings-go/pkg/config"
+	"github.com/aziderius/bookings-go/pkg/models"
+	"bytes"
+	"html/template"
+	"log"
+	"net/http"
+	"path/filepath"
+)
+
+var app *config.AppConfig
+
+// NewTemplates sets the config for the template package
+func NewTemplates(a *config.AppConfig) {
+	app = a
+}
+
+func AddDefaultData(td *models.TemplateData) *models.TemplateData {
+
+	return td
+}
+
+// RenderTemplate renders template using a html/template
+func RenderTemplate(w http.ResponseWriter, tpl string, td *models.TemplateData) {
+	var tc map[string]*template.Template
+
+	if app.UseCache {
+		// get the template cache from the app config
+		tc = app.TemplateCache
+	} else {
+		tc, _ = CreateTemplateCache()
+	}
+
+	/*
+		tc, err := CreateTemplateCache()
+		if err != nil {
+			log.Fatal(err)
+		}*/
+
+	//get requested template from cache
+	t, ok := tc[tpl]
+	if !ok {
+		log.Fatal("Could not get template from template cache")
+	}
+
+	buf := new(bytes.Buffer)
+
+	td = AddDefaultData(td)
+
+	err := t.Execute(buf, td)
+	if err != nil {
+		log.Println(err)
+	}
+
+	//render the template
+	_, err = buf.WriteTo(w)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func CreateTemplateCache() (map[string]*template.Template, error) {
+	myCache := map[string]*template.Template{}
+
+	//get all of the files name *.page.tpl from the ./templates
+	pages, err := filepath.Glob("./templates/*.page.tpl")
+	if err != nil {
+		return myCache, err
+	}
+
+	//range through all files ending with *.page.tpl
+	for _, page := range pages {
+		name := filepath.Base(page)
+		ts, err := template.New(name).ParseFiles(page)
+		if err != nil {
+			return myCache, err
+		}
+
+		matches, err := filepath.Glob("./templates/*.layout.tpl")
+		if err != nil {
+			return myCache, err
+		}
+
+		if len(matches) > 0 {
+			ts, err = ts.ParseGlob("./templates/*.layout.tpl")
+			if err != nil {
+				return myCache, err
+			}
+		}
+
+		myCache[name] = ts
+	}
+
+	return myCache, nil
+
+}
+
+//First Aproach - using a map to cache the templates (for Jr's)
+/*
+var tc = make(map[string]*template.Template)
+
+func RenderTemplate(w http.ResponseWriter, t string) {
+	var tpl *template.Template
+	var err error
+
+	//check to see if we already have the template in our cache
+	_, inMap := tc[t]
+	if !inMap {
+		//need to create the template
+		log.Println("creating template and adding to cache")
+		err = createTemplateCache(t)
+		if err != nil {
+			log.Println(err)
+		}
+	} else {
+		//we have the template in the cache
+		log.Println("using cached template")
+	}
+
+	tpl = tc[t]
+
+	err = tpl.Execute(w, nil)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func createTemplateCache(t string) error {
+	templates := []string{
+		fmt.Sprintf("./templates/%s", t),
+		"./templates/base.layout.tpl",
+	}
+
+	//parse the template
+	tpl, err := template.ParseFiles(templates...)
+	if err != nil {
+		return err
+	}
+
+	//add template to cache (map)
+	tc[t] = tpl
+
+	return nil
+}
+*/
---
+++ templates/home.page.tpl
@@ -0,0 +1,12 @@
+{{template "base" .}}
+
+{{define "content"}}
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1>This is a Home Page</h1>
+                <p>This is a simple text</p>
+            </div>
+        </div>
+    </div>
+{{end}}
