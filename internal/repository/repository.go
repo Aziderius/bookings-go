33ab1111d50c6114ca6ace5c745bb54d73f7d341
Aziderius <143766082+Aziderius@users.noreply.github.com>
2024-10-25 17:30:34 -0700

    handler nuevo para guardar una reservacion en la base de datos

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -5,5 +5,7 @@ import "github.com/aziderius/bookings-go/internal/models"
 type DatabaseRepo interface {
 	AllUsers() bool
 
-	InsertReservation(res models.Reservation) error
+	InsertReservation(res models.Reservation) (int, error)
+
+	InsertRoomRestriction(r models.RoomRestriction) error
 }
